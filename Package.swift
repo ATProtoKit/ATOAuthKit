dda4c14c558300a3c974c013349926ea8a98385c
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-07-23 05:37:31 -0400

    Update Package.swift
    - Removed Package@6.0.0.swift.
    - Upgraded Package.swift to swift-tools-version 6.0.
    - Added the Jot dependency.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -18,27 +18,21 @@ let package = Package(
             name: "ATOAuthKit",
             targets: ["ATOAuthKit"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/mattmassicotte/Jot.git", from: "0.1.1")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ATOAuthKit"),
+            name: "ATOAuthKit",
+            dependencies: [
+                .product(name: "Jot", package: "jot")
+            ]
+        ),
         .testTarget(
             name: "ATOAuthKitTests",
             dependencies: ["ATOAuthKit"]
         ),
     ]
 )
-
-let swiftSettings: [SwiftSetting] = [
-    .enableExperimentalFeature("StrictConcurrency"),
-    .enableUpcomingFeature("DisableOutwardActorInference"),
-    .enableUpcomingFeature("GlobalActorIsolatedTypesUsability"),
-    .enableUpcomingFeature("InferSendableFromCaptures"),
-]
-
-for target in package.targets {
-    var settings = target.swiftSettings ?? []
-    settings.append(contentsOf: swiftSettings)
-    target.swiftSettings = settings
-}
