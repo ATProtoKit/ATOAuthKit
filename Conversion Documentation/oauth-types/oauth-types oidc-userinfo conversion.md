f23f91b06fb4c5e29c52ba3c5d58e2f1521c2436
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-07-25 16:43:01 -0400

    Add model for oidc-userinfo
    Also added the conversion documentation.

---
+++ Conversion Documentation/oauth-types/oauth-types oidc-userinfo conversion.md
@@ -0,0 +1,5 @@
+# Conversion for `oauth-types/src/oidc-userinfo.ts`
+
+## `export const oidcUserinfoSchema`
+
+This has been converted.
---
+++ Sources/OAuthTypesLab/OIDCUserInfo.swift
@@ -0,0 +1,55 @@
+//
+//  OIDCUserInfo.swift
+//  ATOAuthKit
+//
+//  Created by Christopher Jr Riley on 2025-07-25.
+//
+
+import Foundation
+
+/// A structure representing the user information for OpenID Connect.
+public struct OpenIDConnectUserInfo: Codable {
+
+    /// The end user within the identity provider.
+    ///
+    /// This is how the application knows which user has authenticated.
+    public let subject: String
+
+    /// The unique identifier of the identity provider. Optional.
+    ///
+    /// This is so the client can identify who issued the token.
+    public let issuer: URL?
+
+    /// The intended recipient(s) for this token—usually, the client application's identifier (such as its OAuth client ID).
+    ///
+    /// This will ensure the token is being used by the right client. Optional.
+    public let audience: [String]?
+
+    /// The email address of the subject. Optional.
+    public let email: String?
+
+    /// Determines whether the email has been verified. Optional.
+    public let isEmailVerified: Bool?
+
+    /// The subject's name. Optional.
+    public let name: String?
+
+    /// The preferred username of the subject. Optional.
+    public let preferredUsername: String?
+
+    /// A URL of the subject's photo. Optional.
+    ///
+    /// This would typically be used for the profile picture.
+    public let picture: URL?
+
+    enum CodingKeys: String, CodingKey {
+        case subject = "sub"
+        case issuer = "iss"
+        case audience = "aud"
+        case email
+        case isEmailVerified = "email_verified"
+        case name
+        case preferredUsername = "preferred_username"
+        case picture
+    }
+}
