7503213e779806838b5389427df9319f4866b6bd
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-07-26 18:57:59 -0400

    Add enumeration for oauth-response-mode
    Also adds conversion documentation.

---
+++ Conversion Documentation/oauth-types/oauth-types oauth-response-mode conversion.md
@@ -0,0 +1,5 @@
+# Conversion for `oauth-types/src/oauth-response-mode.ts`
+
+## `export const oauthResponseModeSchema`
+
+This has been converted.
---
+++ Sources/OAuthTypesLab/OAuthResponseMode.swift
@@ -0,0 +1,21 @@
+//
+//  OAuthResponseMode.swift
+//  ATOAuthKit
+//
+//  Created by Christopher Jr Riley on 2025-07-26.
+//
+
+/// An enumeration representing the method used to return OAuth authorization responses to
+/// the client.
+public enum OAuthResponseMode: String {
+
+    /// The response parameters are encoded in the query string of the redirect URI.
+    case query
+
+    /// The response parameters are encoded in the fragment portion of the redirect URI.
+    case fragment
+
+    /// The response parameters are encoded as HTML form values and transmitted via HTTP POST
+    /// to the client.
+    case formPost = "form_post"
+}
