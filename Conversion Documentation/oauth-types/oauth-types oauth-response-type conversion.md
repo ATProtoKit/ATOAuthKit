85aabca0dae555cab35b59b083f2dfd7a12cb6fb
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-07-26 17:51:42 -0400

    Add enumeration for oauth-response-type
    Also added conversion documentation.

---
+++ Conversion Documentation/oauth-types/oauth-types oauth-response-type conversion.md
@@ -0,0 +1,5 @@
+# Conversion for `oauth-types/src/oauth-response-type.ts`
+
+## `export const oauthResponseTypeSchema`
+
+This has been converted.
---
+++ Sources/OAuthTypesLab/OAuthResponseType.swift
@@ -0,0 +1,67 @@
+//
+//  OAuthResponseType.swift
+//  ATOAuthKit
+//
+//  Created by Christopher Jr Riley on 2025-07-26.
+//
+
+/// An enumeration representing the allowed OAuth and OpenID Connect response types.
+///
+/// - SeeAlso: \
+/// \* OAuth2: https://datatracker.ietf.org/doc/html/draft-ietf-oauth-v2-1-10#section-4.1.1\
+/// \* OpenID Connect: https://openid.net/specs/oauth-v2-multiple-response-types-1_0.html
+public enum OAuthResponse: String {
+
+    // OAuth2 response types
+    /// An authorization code to be exchanged for tokens.
+    ///
+    /// - Note: OAuth 2.0 Authorization Code Grant\
+    /// Value: `code`
+    case authorizationCodeGrant = "code"
+
+    /// An access token directly from the authorization endpoint.
+    ///
+    /// - Note: OAuth 2.0 Implicit Grant\
+    /// Value: `token`
+    case implicitGrant = "token"
+
+    // OpenID Connect response types
+    /// No token or code is issued.
+    ///
+    /// Used primarily for requests that only perform authentication.
+    ///
+    /// - Note: OpenID Connect\
+    /// Value: `none`
+    case none = "none"
+
+    /// Returns an authorization code, ID token, and access token in a single response.
+    ///
+    /// - Note: OpenID Connect Hybrid Flow\
+    /// Value: `code id_token token`
+    case codeIDTokenToken = "code id_token token"
+
+
+    /// Returns an authorization code and an ID token.
+    ///
+    /// - Note: OpenID Connect Hybrid Flow\
+    /// Value: `code id_token`
+    case codeIDToken = "code id_token"
+
+    /// Returns an authorization code and an access token.
+    ///
+    /// - Note: OpenID Connect Hybrid Flow\
+    /// Value: `code token`
+    case codeToken = "code token"
+
+    /// Returns an ID token and an access token.
+    ///
+    /// - Note: OpenID Connect Implicit or Hybrid Flow\
+    /// Value: `id_token token`
+    case idTokenToken = "id_token token"
+
+    /// Returns only an ID token (no access token or code).
+    ///
+    /// - Note: OpenID Connect Implicit Flow\
+    /// Value: `id_token`
+    case idToken = "id_token"
+}
