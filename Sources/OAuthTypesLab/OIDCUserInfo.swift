b4e1b602b6333f01b07cfbdd6ff636913f524606
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-07-26 05:09:52 -0400

    Add claims to the documentation for user info

--- Sources/OAuthTypesLab/OIDCUserInfo.swift
+++ Sources/OAuthTypesLab/OIDCUserInfo.swift
@@ -10,17 +10,17 @@ import Foundation
 /// A structure representing the user information for OpenID Connect.
 public struct OpenIDConnectUserInfo: Codable {
 
-    /// The end user within the identity provider.
+    /// The end user within the identity provider (the `sub` claim).
     ///
     /// This is how the application knows which user has authenticated.
     public let subject: String
 
-    /// The unique identifier of the identity provider. Optional.
+    /// The unique identifier of the identity provider (the `iss` claim). Optional.
     ///
     /// This is so the client can identify who issued the token.
     public let issuer: URL?
 
-    /// The intended recipient(s) for this token—usually, the client application's identifier (such as its OAuth client ID).
+    /// The intended recipient(s) for this token (the `aud` claim). Optional.
     ///
     /// This will ensure the token is being used by the right client. Optional.
     public let audience: [String]?
@@ -29,12 +29,16 @@ public struct OpenIDConnectUserInfo: Codable {
     public let email: String?
 
     /// Determines whether the email has been verified. Optional.
+    ///
+    /// This fits into the `email_verified` claim.
     public let isEmailVerified: Bool?
 
     /// The subject's name. Optional.
     public let name: String?
 
     /// The preferred username of the subject. Optional.
+    ///
+    /// This fits into the `preferred_username` claim.
     public let preferredUsername: String?
 
     /// A URL of the subject's photo. Optional.
