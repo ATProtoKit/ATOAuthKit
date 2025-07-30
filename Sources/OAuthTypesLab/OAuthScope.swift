decd0402ca4da0136ced585b4412369ab6679eaf
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-07-30 10:34:37 -0400

    Tweak OAuthScope conformance and initializer
    - Changed conformance to CustomStringConvertible.
    - Changed documentation to reflect changes.
    - Made initializer failable and throwable.
    - Encapsulated initializer's implementation into a do-catch
    statement to capture any regex errors (but it shouldn't
    ever fail).

--- Sources/OAuthTypesLab/OAuthScope.swift
+++ Sources/OAuthTypesLab/OAuthScope.swift
@@ -7,35 +7,40 @@
 
 import Foundation
 
-/// An enumeration representing the set of possible OAuth scopes as plain strings.
+/// An structure representing the set of possible OAuth scopes as plain strings.
 ///
 /// Each OAuth scope defines what permissions or access a client is requesting from the user.
 /// This enum provides a helper for validating if a given scope string matches the expected
 /// pattern, making it easier to check user input or API responses when working with OAuth.
-public enum OAuthScope: Equatable, Hashable {
+public struct OAuthScope: CustomStringConvertible {
 
-    /// Validates if the input string matches the required pattern for the OAuth scope.
+    public let rawValue: String
+
+    public var description: String {
+        return rawValue
+    }
+
+    /// Validates the specified raw value, then creates a new instance.
     ///
     /// The input will be valid if it includes a space-separated text of non-control ASCII characters, with
     /// the exception of a backslash (`\`) or double quote (`"`).
     ///
-    /// Returns `true` if valid,  or `false` if not.
-    public static func isValid(_ scopeInput: String) -> Bool {
-        let pattern = #"^[\x21\x23-\x5B\x5D-\x7E]+(?: [\x21\x23-\x5B\x5D-\x7E]+)*$"#
-
+    /// - Parameter rawValue: The raw value to validate and use for the new instance.
+    public init?(rawValue: String) throws {
         do {
-            let regex = try NSRegularExpression(pattern: pattern)
-            let range = NSRange(scopeInput.startIndex..<scopeInput.endIndex, in: scopeInput)
+            let pattern = #"^[\x21\x23-\x5B\x5D-\x7E]+(?: [\x21\x23-\x5B\x5D-\x7E]+)*$"#
 
-            if let match = regex.firstMatch(in: scopeInput, options: [], range: range) {
-                return match.range == range
+            let regex = try NSRegularExpression(pattern: pattern)
+            let range = NSRange(rawValue.startIndex..<rawValue.endIndex, in: rawValue)
+
+            if let match = regex.firstMatch(in: rawValue, options: [], range: range) {
+                self.rawValue = rawValue
             } else {
-                return false
+                return nil
             }
         } catch {
-            // If the regex fails for whatever reason, treat this as false. However, this should
-            // never happen.
-            return false
+            // If the regex fails for whatever reason, throw an error. However, this should never happen.
+            throw error
         }
     }
 }
